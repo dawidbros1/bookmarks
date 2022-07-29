690c6eaafa7ae1f7f5149da58c5d04891b574cee
dawidbros1 <dawidbros1@wp.pl>
2022-07-29 09:38:46 +0200

    package images into folders

--- templates/general/home.php
+++ templates/general/home.php
@@ -10,43 +10,43 @@
    <div class="justify-center d-flex flex-wrap">
       <div class="box-item">
          <div class="title">Szybki dostęp do ulubionych stron</div>
-         <img src="./public/images/fast.jpg" />
+         <img src="./public/images/Home/fast.jpg" />
       </div>
 
       <div class="box-item">
          <div class="title">Dostęp z każdego urządzenia</div>
-         <img src="./public/images/responsive.png" />
+         <img src="./public/images/Home/responsive.png" />
       </div>
 
       <div class="box-item">
          <div class="title">Graficzny interfejs</div>
-         <img src="./public/images/interface.jpg" />
+         <img src="./public/images/Home/interface.jpg" />
       </div>
 
       <div class="box-item">
          <div class="title">Udostępniaj zawartości znajomym</div>
-         <img src="./public/images/sharing.jpg" />
+         <img src="./public/images/Home/sharing.jpg" />
       </div>
 
       <div class="box-item">
          <div class="title">Umieszczaj elementy w folderach</div>
-         <img src="./public/images/subfolders.jpg" />
+         <img src="./public/images/Home/subfolders.jpg" />
       </div>
 
       <div class="box-item">
          <div class="title">Dowolnie rozmieszczaj elementy</div>
-         <img src="./public/images/sort.jpg" />
+         <img src="./public/images/Home/sort.jpg" />
       </div>
    </div>
 </div>
 
-<?php if (!$user) : ?>
+<?php if (!$user): ?>
    <div class="px-6 py-1 mt-4 register pb-4">
       <div class="text-center"> Nie masz jeszcze konta? </div>
-      <a href="<?= $route->get('auth.register') ?>">
+      <a href="<?=$route->get('auth.register')?>">
          <button class="bg-primary text-white font-bold py-2 px-4 rounded">
             Zarejestruj się
          </button>
       </a>
    </div>
-<?php endif; ?>
+<?php endif;?>
