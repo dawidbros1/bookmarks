8d57cff55d2c158f3d9f77c9549f02ee99239693
dawidbros1 <dawidbros1@wp.pl>
2022-08-03 11:21:16 +0200

    added html section for category.list without content

--- templates/category/list.php
+++ templates/category/list.php
@@ -1,8 +1,8 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types = 1);
 
-use App\Component;
+use App\Component\Component;
 
 $categories = $params['categories'];
 
@@ -10,8 +10,15 @@ $categories = $params['categories'];
 
 <div class="p-3 pt-2">
     <div class="row">
-        <?php foreach ($categories as $index => $category) : ?>
-            <?php Component::render('item.item', ['item' => $category, 'model' => "category", 'route' => $route, 'url' => $params['url'], 'index' => $index]) ?>
-        <?php endforeach; ?>
+        <?php foreach ($categories as $index => $category): ?>
+            <?php Component::render('item.category', ['item' => $category, 'route' => $route, 'url' => $params['url'], 'index' => $index])?>
+        <?php endforeach;?>
+
+        <?php if (empty($categories)): ?>
+        <div>
+            <h2 class = "text-center">Tu jeszcze nic nie ma.</h2>
+            <h2 class = "text-center">Dodaj swoją pierwszą <a class="text-decoration-none" href = "<?=$route->get('category.create')?>">kategorię</a></h2>
+        </div>
+        <?php endif?>
     </div>
 </div>
