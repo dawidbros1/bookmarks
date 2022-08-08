6e37c477caf184d7d5f9095021eb041a774277ba
dawidbros1 <dawidbros1@wp.pl>
2022-08-08 12:15:17 +0200

    update in item.form.delete

--- templates/component/item/form/delete.php
+++ templates/component/item/form/delete.php
@@ -1,9 +1,9 @@
-<div class="collapse border-top <?=$params['class'] ?? ""?>">
+<div class="collapse border-top border-2 pt-2 mt-3 delete <?=$params['class'] ?? ""?>">
     <p class = "text-center fw-bold"> Czy jesteś pewien, że chcesz usunąć wybrany element? </p>
 
-    <form class = "d-flex" action = "<?=$params['action']?>" method = "POST">
+    <form class = "d-flex border-bottom border-2 pb-2" action = "<?=$params['action']?>" method = "POST">
         <input type = "hidden" name = "id" value = "<?=$params['id']?>">
-        <button data-bs-toggle="collapse" data-bs-target="<?=$params['target']?>" class="btn btn-success col-5 fw-bold" type = "button"> Nie </button>
+        <button data-bs-toggle="collapse" data-bs-target=".btn1,.btn2,.delete" class="btn btn-success col-5 fw-bold" type = "button"> Nie </button>
         <button class="btn btn-danger col-5 offset-2 fw-bold" type = "submit"> Tak </button>
     </form>
 </div>
