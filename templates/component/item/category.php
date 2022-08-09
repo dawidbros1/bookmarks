04ea1c1071a3e39d99963980daf2f28a5868d70f
dawidbros1 <dawidbros1@wp.pl>
2022-08-09 12:12:40 +0200

    refactoring

--- templates/component/item/category.php
+++ templates/component/item/category.php
@@ -2,12 +2,9 @@
 
 declare (strict_types = 1);
 
-$category = $params['item'];
+$category = $params['category'];
 $route = $params['route'];
-
-$manage = $params['manage'] ?? true; // Public data and Create page can't have settings icon
-$index = $params['index'] ?? null; // Category class [ copy ] counter
-$url = $params['url'] ?? null; // Copy public link to category
+$link = $params['location'] . $route->get('category.public'); // link to (category.public) without param id
 
 ?>
 
@@ -15,12 +12,11 @@ $url = $params['url'] ?? null; // Copy public link to category
     <div class="name text-center"> <?=$category->name;?></div>
 
     <div class="position-relative">
-         <?php if ($manage == true): ?>
-               <a href="<?=$route->get('category.edit') . "&id=$category->id"?>"><img class="icon icon-settings" src="public/images/Item/settings.png" alt="setting-icon"></img></a>
-         <?php endif;?>
+         <a href="<?=$route->get('category.edit') . "&id=$category->id"?>"><img class="icon icon-settings" src="public/images/Item/settings.png" alt="setting-icon"></img></a>
+         <a href="<?=$route->get('category.show') . "&id=$category->id"?>"><img class="image" src="<?=$category->image?>" alt="category-image"></img></a>
 
-         <a href="<?=$route->get('category.show') . "&id=" . $category->id?>"><img class="image" src="<?=$category->image?>" alt="category-image"></img></a>
-         <img src="public/images/Item/copy.png" alt="copy-image" class="icon icon-copy <?=$category->private ? "d-none" : ""?>" title="Skopiuj link do udostępnienia" onclick="copyToClipBoard(<?=$index?>)">
-         <input type="hidden" class="copy" value="<?=$url . $route->get('category.public') . "&id=" . $category->id?>">
+        <?php if ($category->private == 0): ?>
+            <img src="public/images/Item/copy.png" alt="copy-image" data-link = "<?=$link . "&id=" . $category->id?>" class="icon icon-copy" title="Skopiuj link do udostępnienia" onclick="copyToClipBoard(this)">
+        <?php endif;?>
     </div>
 </div>
--- templates/component/item/page.php
+++ templates/component/item/page.php
@@ -2,26 +2,18 @@
 
 declare (strict_types = 1);
 
-$page = $params['item'];
-$route = $params['route'];
-
-$manage = $params['manage'] ?? true; // Public data and Create page can't have settings icon
-$create = $params['create'] ?? false; // Show create page item
+$page = $params['page'];
 
 ?>
 
 <div class="item col-6 col-sm-4 col-md-3 col-lg-2 col-xl-1">
-    <div class="name text-center"> <?=$create == true ? "Dodaj stronę" : $page->name;?></div>
+    <div class="name text-center"> <?=$page->name?></div>
 
     <div class="position-relative">
-         <?php if ($create == true): ?>
-               <a href="<?=$route->get('page.create') . "&category_id=" . $page->id?>"><img class="image bg-light p-3" src="public/images/Item/plus.png" alt="create-page-image"></img></a>
-         <?php else: ?>
-               <a href="<?=$page->link?>" target="_blank"><img class="image bg-light" src="<?=$item->image?>" alt="page-image"></img></a>
-         <?php endif;?>
+        <a href="<?=$page->link?>" target="<?=$params['target'] ?? ""?>"><img class="image bg-light <?=$params['class'] ?? ""?>" src="<?=$page->image?>" alt="page-image"></img></a>
 
-        <?php if ($create == false && $manage == true): ?>
-            <a href="<?=$route->get('page.edit') . "&id=$page->id"?>"><img class="icon icon-settings" src="public/images/Item/settings.png" alt="setting-icon"></img></a>
+        <?php if ($params['manage'] == true): ?>
+            <a href="<?=$params["route"]->get('page.edit') . "&id=$page->id"?>"><img class="icon icon-settings" src="public/images/Item/settings.png" alt="setting-icon"></img></a>
         <?php endif;?>
     </div>
 </div>
