fea9f11225740de40254a4e7d571e2440af9990b
dawidbros1 <dawidbros1@wp.pl>
2022-08-09 12:17:41 +0200

    update

--- templates/category/show.php
+++ templates/category/show.php
@@ -5,7 +5,7 @@ declare (strict_types = 1);
 use App\Component\Component;
 
 $category = $params['category'];
-$manage = $params['manage'] ?? false;
+$manage = $params['manage'];
 
 ?>
 
@@ -13,12 +13,14 @@ $manage = $params['manage'] ?? false;
     <div class="row">
         <div class="col-12 text-center p-1 bg-light"><?=$category->name?></div>
 
+        <!-- ITEM => Create Page -->
         <?php if ($manage == true): ?>
-            <?php Component::render('item.page', ['item' => $category, 'route' => $route, 'create' => true])?>
+            <?php Component::render('item.page', ['class' => "p-3", 'page' => $category->pages[0], 'route' => $route, 'manage' => false])?>
+            <?php array_shift($category->pages)?>
         <?php endif;?>
 
         <?php foreach ($category->pages as $page): ?>
-            <?php Component::render('item.page', ['item' => $page, 'route' => $route, 'manage' => $manage])?>
+            <?php Component::render('item.page', ['page' => $page, 'route' => $route, 'manage' => $manage, 'target' => "_blank"])?>
         <?php endforeach;?>
     </div>
 </div>
